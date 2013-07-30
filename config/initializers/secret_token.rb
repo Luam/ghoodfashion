5a76e7df6fa1192c7377e8885b6052f6ff3eb972
Luam <luam13@gmail.com>
2013-07-30 16:04:55 -0400

    My initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ghoodfashion::Application.config.secret_token = '26f58d5d98a123afc6fb2312a04e3ac11f47ba86f4306078c1dbcd8ed5d60cdf3ad98df822c4b685623538163a97abf426a0c5ed1be848de88c6fbc8009fa4a0'
